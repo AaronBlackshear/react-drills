ae4e588940d8d3509c9f0d89337bbba97f10ec96
Aaron Blackshear <theshiftybapple@gmail.com>
2018-03-20 07:56:47 -0500

    Finished App 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image img={'https://images.pexels.com/photos/691582/pexels-photo-691582.jpeg?w=940&h=650&auto=compress&cs=tinysrgb'} />
       </div>
     );
   }
